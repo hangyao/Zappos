952b0cb3cdc04a91a36b32f4cf979c068700dd40
Hang Yao <hang_yao@hotmail.com>
2016-07-24 02:04:29 -0500

    feat: updated ipynb and renders.py files

--- renders.py
+++ renders.py
@@ -52,12 +52,13 @@ def cluster_results(reduced_data, preds, centers, pca_samples):
 	fig, ax = plt.subplots(figsize = (14,8))
 
 	# Color map
-	cmap = cm.get_cmap('gist_rainbow')
+	cmap = cm.get_cmap('Accent')
 
 	# Color the points based on assigned cluster
 	for i, cluster in plot_data.groupby('Cluster'):   
 	    cluster.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
-	                 color = cmap((i)*1.0/(len(centers)-1)), label = 'Cluster %i'%(i), s=30);
+	                 color = cmap((i)*1.0/len(centers)), label = 'Cluster %i'%(i), s=30);
+	ax.set_xlim(-10, 25)
 
 	# Plot centers with indicators
 	for i, c in enumerate(centers):
@@ -72,6 +73,58 @@ def cluster_results(reduced_data, preds, centers, pca_samples):
 	# Set plot title
 	ax.set_title("Cluster Learning on PCA-Reduced Data - Centroids Marked by Number\nTransformed Sample Data Marked by Black Cross");
 
+
+def site_results(reduced_data, data, pca_samples):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions using the full dataset
+	Data is labeled by "Channel" and cues added for student-selected sample data
+	'''
+
+	# Check that the dataset is loadable
+	try:
+	    full_data = data
+	except:
+	    print "Dataset could not be loaded. Is the file missing?"
+	    return False
+
+	# Create the Channel DataFrame
+	channel = pd.DataFrame(full_data['site'], columns = ['site'])
+	# channel = channel.drop(channel.index[outliers]).reset_index(drop = True)
+	labeled = pd.concat([reduced_data, channel], axis = 1)
+	
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Color map
+	cmap = cm.get_cmap('Paired')
+
+	# Color the points based on assigned Channel
+	labels = ['Acme', 'Botly', 'Pinnacle', 'Sortly', 'Tabular', 'Widgetry']
+	grouped = labeled.groupby('site')
+	for i, channel in grouped: 
+	    if i == 'Acme': 
+		j = 1
+	    elif i == 'Botly':
+		j = 2
+	    elif i == 'Pinnacle':
+		j = 3
+	    elif i == 'Sortly':
+		j = 4
+	    elif i == 'Tabular':
+		j = 5
+	    else:
+		j = 6
+	    channel.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((j-1)*1.0/6), label = labels[j-1], s=30);
+	ax.set_xlim(-10, 25)
+	# Plot transformed sample points   
+	for i, sample in enumerate(pca_samples):
+		ax.scatter(x = sample[0], y = sample[1], \
+	           s = 200, linewidth = 3, color = 'black', marker = 'o', facecolors = 'none');
+		ax.scatter(x = sample[0]+0.25, y = sample[1]+0.3, marker='$%d$'%(i), alpha = 1, s=125);
+
+	# Set plot title
+	ax.set_title("PCA-Reduced Data Labeled by 'site'\nTransformed Sample Data Circled");
 
 def channel_results(reduced_data, data, pca_samples):
 	'''
@@ -95,7 +148,7 @@ def channel_results(reduced_data, data, pca_samples):
 	fig, ax = plt.subplots(figsize = (14,8))
 
 	# Color map
-	cmap = cm.get_cmap('gist_rainbow')
+	cmap = cm.get_cmap('Paired')
 
 	# Color the points based on assigned Channel
 	labels = ['Returning', 'New', 'Neither']
@@ -108,8 +161,9 @@ def channel_results(reduced_data, data, pca_samples):
 	    else:
 		j = 3
 	    channel.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
-	                 color = cmap((j-1)*1.0/2), label = labels[j-1], s=30);
-	    
+	                 color = cmap((j-1)*1.0/3), label = labels[j-1], s=30);
+	ax.set_xlim(-10, 25)
+
 	# Plot transformed sample points   
 	for i, sample in enumerate(pca_samples):
 		ax.scatter(x = sample[0], y = sample[1], \
@@ -117,4 +171,75 @@ def channel_results(reduced_data, data, pca_samples):
 		ax.scatter(x = sample[0]+0.25, y = sample[1]+0.3, marker='$%d$'%(i), alpha = 1, s=125);
 
 	# Set plot title
-	ax.set_title("PCA-Reduced Data Labeled by 'Channel'\nTransformed Sample Data Circled");
+	ax.set_title("PCA-Reduced Data Labeled by 'new_customer'\nTransformed Sample Data Circled");
+
+
+def platform_results(reduced_data, data, pca_samples):
+	'''
+	Visualizes the PCA-reduced cluster data in two dimensions using the full dataset
+	Data is labeled by "Channel" and cues added for student-selected sample data
+	'''
+
+	# Check that the dataset is loadable
+	try:
+	    full_data = data
+	except:
+	    print "Dataset could not be loaded. Is the file missing?"
+	    return False
+
+	# Create the Channel DataFrame
+	channel = pd.DataFrame(full_data['platform'], columns = ['platform'])
+	# channel = channel.drop(channel.index[outliers]).reset_index(drop = True)
+	labeled = pd.concat([reduced_data, channel], axis = 1)
+	
+	# Generate the cluster plot
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Color map
+	cmap = cm.get_cmap('Set1')
+
+	# Color the points based on assigned Channel
+	labels = ['Android', 'BlackBerry', 'ChromeOS', 'Linux', 'MacOSX', 'Macintosh', 'Other',
+                 'SymbianOS', 'Unknown', 'Windows', 'WindowsPhone', 'iOS', 'iPad', 'iPhone']
+	grouped = labeled.groupby('platform')
+	for i, channel in grouped: 
+	    if i == 'Android': 
+		j = 1
+	    elif i == 'BlackBerry':
+		j = 2
+	    elif i == 'ChromeOS':
+		j = 3
+	    elif i == 'Linux':
+		j = 4
+	    elif i == 'MacOSX':
+		j = 5
+	    elif i == 'Macintosh':
+		j = 6
+	    elif i == 'Other':
+		j = 7
+	    elif i == 'SymbianOS':
+		j = 8
+	    elif i == 'Unknown':
+		j = 9
+	    elif i == 'Windows':
+		j = 10
+	    elif i == 'WindowsPhone':
+		j = 11
+	    elif i == 'iOS':
+		j = 12
+	    elif i == 'iPad':
+		j = 13
+	    else:
+		j = 14
+	    channel.plot(ax = ax, kind = 'scatter', x = 'Dimension 1', y = 'Dimension 2', \
+	                 color = cmap((j-1)*1.0/14), label = labels[j-1], s=30);
+	ax.set_xlim(-10, 25)
+
+	# Plot transformed sample points   
+	for i, sample in enumerate(pca_samples):
+		ax.scatter(x = sample[0], y = sample[1], \
+	           s = 200, linewidth = 3, color = 'black', marker = 'o', facecolors = 'none');
+		ax.scatter(x = sample[0]+0.25, y = sample[1]+0.3, marker='$%d$'%(i), alpha = 1, s=125);
+
+	# Set plot title
+	ax.set_title("PCA-Reduced Data Labeled by 'platform'\nTransformed Sample Data Circled");
